afe3830420def6d8d673dc254bac1d4a228cfe51
prabhakarrathore <prabhakar0to0@gmail.com>
2025-08-30 12:59:29 +0530

    testing

--- src/util/apiConfig.ts
+++ src/util/apiConfig.ts
@@ -1,3 +1,3 @@
 const config = {
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://fosterai-backend.onrender.com/api'
-  };export default config;
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://fosterai-backend.onrender.com/api'
+}; export default config;
